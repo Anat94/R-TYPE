a7dae7e43a7dd2be0885fd3849c3716a74e1acc6
noah.tesson <noah.tesson@epitech.eu>
2023-11-29 15:05:25 +0100

    [ADD] basic server

--- src/Server/main.cpp
+++ src/Server/main.cpp
@@ -1,3 +1,17 @@
-int main(int argc, char** argv) {
+/*
+** EPITECH PROJECT, 2023
+** rtype
+** File description:
+** main
+*/
+
+#include "Server.hpp"
+#include <cstdio>
+
+int main() {
+
+    Server server(3000, MAX_NB_USER);
+
+    server.startListening();
     return 0;
 }
