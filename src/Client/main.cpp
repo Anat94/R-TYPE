46ab0cba1fd840d9170b1f1995521f0a28631106
Anatole <anatole.babin@gmail.com>
2023-12-13 15:18:15 +0100

    (client) [FIX] fix connection & refacto code

--- src/Client/main.cpp
+++ src/Client/main.cpp
@@ -10,36 +10,36 @@
 
 using boost::asio::ip::udp;
 
-const int SERVER_PORT = 8888;
+void send_datas(udp::socket& socket, udp::endpoint& server_endpoint)
+{
+    std::string message;
+    std::getline(std::cin, message);
+    socket.send_to(boost::asio::buffer(message), server_endpoint);
+}
 
-int main() {
+void receive_datas(udp::socket& socket, udp::endpoint& server_endpoint)
+{
+    char receive_buffer[1024];
+    udp::endpoint sender_endpoint;
+    size_t bytes_received = socket.receive_from(boost::asio::buffer(receive_buffer), sender_endpoint);
+    receive_buffer[bytes_received] = '\0';
+    std::cout << "Received from " << sender_endpoint.address().to_string() << ": " << receive_buffer << std::endl;
+}
+
+int main(int argc, char** argv) {
+    if (argc != 3) {
+        std::cerr << "Usage: ./client <server_ip>" << std::endl;
+        return 184;
+    }
     try {
         boost::asio::io_context io_context;
-
-        // Create a UDP socket
         udp::socket socket(io_context, udp::endpoint(udp::v4(), 0));
-
-        // Server endpoint
-        udp::endpoint server_endpoint(boost::asio::ip::address::from_string("10.68.255.58"), SERVER_PORT);
-
+        udp::endpoint server_endpoint(boost::asio::ip::address::from_string(argv[1]), std::atoi(argv[2]));
         std::cout << "Enter a message to send (Press Ctrl+C to exit):\n";
 
         while (true) {
-            std::string message;
-            std::getline(std::cin, message);
-
-            // Send data to the server
-            socket.send_to(boost::asio::buffer(message), server_endpoint);
-
-            // Buffer to receive data
-            char receive_buffer[1024];
-            udp::endpoint sender_endpoint;
-
-            // Receive data from the server
-            size_t bytes_received = socket.receive_from(boost::asio::buffer(receive_buffer), sender_endpoint);
-
-            // Output received data
-            std::cout << "Received from " << sender_endpoint.address().to_string() << ": " << receive_buffer << std::endl;
+            send_datas(socket, server_endpoint);
+            receive_datas(socket, server_endpoint);
         }
     } catch (std::exception& e) {
         std::cerr << "Exception: " << e.what() << std::endl;
--- src/Server/main.cpp
+++ src/Server/main.cpp
@@ -12,30 +12,26 @@ using boost::asio::ip::udp;
 
 const int PORT_NUMBER = 8888;
 
-
-int main() {
+int main()
+{
     try {
         boost::asio::io_context io_context;
-
-        // Create a UDP socket
         udp::endpoint endpoint(boost::asio::ip::make_address("127.0.0.1"), PORT_NUMBER);
         udp::socket socket(io_context, endpoint);
-        
         std::cout << "UDP Server listening on port " << PORT_NUMBER << "...\n";
 
         while (true) {
-            // Buffer to store received data
             char receive_buffer[1024];
             udp::endpoint remote_endpoint;
 
-            // Receive data
             size_t bytes_received = socket.receive_from(boost::asio::buffer(receive_buffer), remote_endpoint);
+            receive_buffer[bytes_received] = '\0';
 
-            // Output received data
             std::cout << "Received from " << remote_endpoint.address().to_string() << ": " << receive_buffer << std::endl;
 
-            // Echo the received data back to the client
-            socket.send_to(boost::asio::buffer(receive_buffer, bytes_received), remote_endpoint);
+            std::string message;
+            std::getline(std::cin, message);
+            socket.send_to(boost::asio::buffer(message, message.size()), remote_endpoint);
         }
     } catch (std::exception& e) {
         std::cerr << "Exception: " << e.what() << std::endl;
