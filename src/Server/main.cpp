df51875cc61345692dc14b13fc1d130d8535a3e2
Anatole <anatole.babin@gmail.com>
2023-12-13 16:01:52 +0100

    (server) [FIX] refacto code to clean it

--- src/Server/main.cpp
+++ src/Server/main.cpp
@@ -10,28 +10,38 @@
 
 using boost::asio::ip::udp;
 
-const int PORT_NUMBER = 8888;
-
-int main()
+void send_datas(udp::socket& socket, udp::endpoint& remote_endpoint)
 {
+    std::string message;
+    std::getline(std::cin, message);
+    socket.send_to(boost::asio::buffer(message, message.size()), remote_endpoint);
+}
+
+void receive_datas(udp::socket& socket, udp::endpoint& remote_endpoint)
+{
+    char receive_buffer[1024];
+    size_t bytes_received = socket.receive_from(boost::asio::buffer(receive_buffer), remote_endpoint);
+    receive_buffer[bytes_received] = '\0';
+    std::cout << "Received from " << remote_endpoint.address().to_string() << ": " << receive_buffer << std::endl;
+}
+
+
+int main(int argc, char** argv)
+{
+    if (argc != 3) {
+        std::cerr << "Usage: ./server <server_ip>" << std::endl;
+        return 184;
+    }
     try {
         boost::asio::io_context io_context;
-        udp::endpoint endpoint(boost::asio::ip::make_address("127.0.0.1"), PORT_NUMBER);
-        udp::socket socket(io_context, endpoint);
-        std::cout << "UDP Server listening on port " << PORT_NUMBER << "...\n";
+        udp::endpoint remote_endpoint(boost::asio::ip::make_address(argv[1]), std::atoi(argv[2]));
+        udp::socket socket(io_context, remote_endpoint);
+        std::cout << "UDP Server listening on port " << std::atoi(argv[2]) << "...\n";
 
         while (true) {
-            char receive_buffer[1024];
-            udp::endpoint remote_endpoint;
+            receive_datas(socket, remote_endpoint);
+            send_datas(socket, remote_endpoint);
 
-            size_t bytes_received = socket.receive_from(boost::asio::buffer(receive_buffer), remote_endpoint);
-            receive_buffer[bytes_received] = '\0';
-
-            std::cout << "Received from " << remote_endpoint.address().to_string() << ": " << receive_buffer << std::endl;
-
-            std::string message;
-            std::getline(std::cin, message);
-            socket.send_to(boost::asio::buffer(message, message.size()), remote_endpoint);
         }
     } catch (std::exception& e) {
         std::cerr << "Exception: " << e.what() << std::endl;
