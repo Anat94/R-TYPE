bf05b7880fc89ea22b1a141533949576f120b7cb
Titouan Chesneau <titouan.chesneau@epitech.eu>
2023-12-22 12:25:06 +0100

    (ECS)[ADD] CollisionSystem class

---
+++ src/Ecs/Systems/CollisionSystem.hpp
@@ -0,0 +1,48 @@
+/*
+** EPITECH PROJECT, 2023
+** R-Type
+** File description:
+** CollisionSystem
+*/
+
+#ifndef COLLISIONSYSTEM_HPP_
+#define COLLISIONSYSTEM_HPP_
+#include "Systems.hpp"
+#include "../Events.hpp"
+
+class CollisionSystem {
+    public:
+        CollisionSystem(rtype::event::EventListener *listener) : _listener(listener) {};
+
+        void operator()(sparse_array<component::Drawable> &dra, sparse_array<component::Position> &pos) {
+            for (auto &&[first_ent_idx, d1, p1] : zipper<sparse_array<component::Drawable>, sparse_array<component::Position>>(dra, pos)) {
+                if (!d1.has_value() || !p1.has_value()) continue;
+                for (auto &&[second_ent_idx, d2, p2] : zipper<sparse_array<component::Drawable>, sparse_array<component::Position>>(dra, pos)) {
+                    if (first_ent_idx == second_ent_idx)
+                        continue;
+                    if ((p1->x <= p2->x &&
+                        p1->y <= p2->y &&
+                        (p1->x + 100) >= p2->x &&
+                        (p1->y + 100) >= p2->y) ||
+                        (p2->x <= p1->x &&
+                        p2->y <= p1->y &&
+                        (p2->x + 100) >= p1->x &&
+                        (p2->y + 100) >= p1->y)) {
+                            rtype::event::CollisionEvent* new_event = new rtype::event::CollisionEvent(second_ent_idx, first_ent_idx);
+                        if (_listener->hasEvent(new_event)) {
+                            second_ent_idx++;
+                            delete new_event;
+                            continue;
+                        } else
+                            _listener->addEvent(new_event);
+                    } else {
+
+                    }
+                }
+            }
+        };
+    private:
+        rtype::event::EventListener *_listener;
+};
+
+#endif /* !COLLISIONSYSTEM_HPP_ */
