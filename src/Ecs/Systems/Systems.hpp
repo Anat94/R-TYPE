6444d03669eb43c860a5bc992b057142ae5e4beb
Titouan Chesneau <titouan.chesneau@epitech.eu>
2023-12-22 12:21:35 +0100

    (ECS)[ADD] ISystems interface for class systems

---
+++ src/Ecs/Systems/Systems.hpp
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2023
+** R-Type
+** File description:
+** ISystems
+*/
+
+#ifndef SYSTEMS_HPP_
+#define SYSTEMS_HPP_
+#include "../ZipperIterator.hpp"
+#include "../Registry.hpp"
+
+class ISystems {
+    public:
+        virtual ~ISystems() = default;
+};
+
+#endif /* !SYSTEMS_HPP_ */
