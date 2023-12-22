6c43c5a66b2f7869cde8707cb9dc0bb9ffd81f42
Titouan Chesneau <titouan.chesneau@epitech.eu>
2023-12-22 12:24:49 +0100

    (ECS)[ADD] ControlSystem class

---
+++ src/Ecs/Systems/ControlSystem.hpp
@@ -0,0 +1,39 @@
+/*
+** EPITECH PROJECT, 2023
+** R-Type
+** File description:
+** ControlSystem
+*/
+
+#ifndef CONTROLSYSTEM_HPP_
+#define CONTROLSYSTEM_HPP_
+#include "Systems.hpp"
+#include "../Events.hpp"
+
+class ControlSystem : public ISystems {
+    public:
+        ControlSystem(rtype::event::EventListener *listener) : _listener(listener) {};
+
+        void operator()(sparse_array<component::Velocity> &vel, sparse_array<component::Controllable> &con, component::DrawableContent& content) {
+            for (auto &&[idx, v, c] : zipper<sparse_array<component::Velocity>, sparse_array<component::Controllable>>(vel, con)) {
+                if (c.has_value() && v.has_value()) {
+                    if (content.event->type == sf::Event::KeyPressed) {
+                        if (content.event->key.code == sf::Keyboard::Up)
+                            v->_dy = -30;
+                        if (content.event->key.code == sf::Keyboard::Down)
+                            v->_dy = 30;
+                        if (content.event->key.code == sf::Keyboard::Left)
+                            v->_dx = -30;
+                        if (content.event->key.code == sf::Keyboard::Right)
+                            v->_dx = 30;
+                        if (content.event->key.code == sf::Keyboard::Space)
+                            _listener->addEvent(new rtype::event::ShootEvent(idx, -1));
+                    }
+                }
+            }
+        };
+    private:
+        rtype::event::EventListener *_listener;
+};
+
+#endif /* !CONTROLSYSTEM_HPP_ */
