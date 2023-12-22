15cf47d56eca8abcf15193de6988478f2996ae84
Titouan Chesneau <titouan.chesneau@epitech.eu>
2023-12-22 12:22:56 +0100

    (ECS)[ADD] RotationSystem class

---
+++ src/Ecs/Systems/RotationSystem.hpp
@@ -0,0 +1,25 @@
+/*
+** EPITECH PROJECT, 2023
+** R-Type
+** File description:
+** RotationSystem
+*/
+
+#ifndef ROTATIONSYSTEM_HPP_
+#define ROTATIONSYSTEM_HPP_
+#include "Systems.hpp"
+
+class RotationSystem : public ISystems {
+    public:
+        RotationSystem() {};
+
+        void operator()(sparse_array<component::Drawable> &dra, sparse_array<component::Rotation> &rot) {
+            for (auto &&[_, d, r] : zipper<sparse_array<component::Drawable>, sparse_array<component::Rotation>>(dra, rot)) {
+                if (d.has_value() && r.has_value())
+                    d->_sprite.setRotation(r->_degrees);
+            }
+        };
+    private:
+};
+
+#endif /* !ROTATIONSYSTEM_HPP_ */
