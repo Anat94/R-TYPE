d6ca9f02dc379ff92617e3b6429e3df5e9f49105
Anatole <anatole.babin@gmail.com>
2023-12-26 21:38:24 +0100

    (unit test) [RM] unused files

--- mongo.sh
+++
@@ -1,6 +0,0 @@
-cd mongo-cxx-driver-r3.9.0/build
-cmake ..                                \
-    -DCMAKE_BUILD_TYPE=Release          \
-    -DMONGOCXX_OVERRIDE_DEFAULT_INSTALL_PREFIX=OFF
-cmake --build .
-sudo cmake --build . --target install
