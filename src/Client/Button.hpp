fef0e3a2add2132a11b095e38ffc5bec0c46e23a
noah.tesson <noah.tesson@epitech.eu>
2023-12-28 15:48:36 +0100

    (CLIENT)[ADD] operaotr () in class buttonSystem

--- src/Client/Button.hpp
+++ src/Client/Button.hpp
@@ -12,6 +12,28 @@
 class Button {
     public:
         Button(float x, float y, float width, float height, sf::Font& font, std::string text, sf::Color textColor, sf::Color idleColor, sf::Color hoverColor, sf::Color activeColor);
+
+        void operator()(sparse_array<component::Hitbox> &dra, sparse_array<component::Position> &pos) {
+            for (auto &&[first_ent_idx, h1, p1] : zipper<sparse_array<component::Hitbox>, sparse_array<component::Position>>(dra, pos)) {
+                if (!h1.has_value() || !p1.has_value()) continue;
+                for (auto &&[second_ent_idx, h2, p2] : zipper<sparse_array<component::Hitbox>, sparse_array<component::Position>>(dra, pos)) {
+                    if (first_ent_idx == second_ent_idx || !h2.has_value() || !p2.has_value() || *h1 == *h2)
+                        continue;
+                    auto temp_h1 = h1->update(*p1);
+                    auto temp_h2 = h2->update(*p2);
+                    if (temp_h1.isTouching(temp_h2) || temp_h2.isTouching(temp_h1)) {
+                        CollisionEvent* new_event = new CollisionEvent(first_ent_idx + 1, second_ent_idx);
+                        if (_listener->hasEvent(new_event)) {
+                            second_ent_idx++;
+                            delete new_event;
+                            continue;
+                        } else
+                            _listener->addEvent(new_event);
+                    }
+                }
+            }
+        };
+
         ~Button();
         void update(const sf::Vector2f& mousePos);
         void render(sf::RenderWindow& window);
@@ -20,6 +42,7 @@ class Button {
         
 
     private:
+        EventListener *_listener;
         sf::RectangleShape shape;
         sf::Text text;
         sf::Color textColor;
