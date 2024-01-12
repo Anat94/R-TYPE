93e71ad8c69a3410718d2a65dd615dfbecb461b9
Anat94 <Anat94@users.noreply.github.com>
2024-01-12 22:54:10 +0000

    deploy: 16aa3f02bed2602bde98347ece4eb968f8f4701c

--- search/functions_16.js
+++ search/functions_16.js
@@ -1,7 +1,5 @@
 var searchData=
 [
-  ['_7ebasic_5fjson_0',['~basic_json',['../classbasic__json.html#a9f3bcb6dc54f447ad95085715104494e',1,'basic_json']]],
-  ['_7eeventlistener_1',['~EventListener',['../classEventListener.html#a4984b3727ac41de6cdfa875dbc05580d',1,'EventListener']]],
-  ['_7eievent_2',['~IEvent',['../classIEvent.html#a37c201e7a45ac21a4ad0e387a6d9b47d',1,'IEvent']]],
-  ['_7esparse_5farray_3',['~sparse_array',['../classsparse__array.html#a462f64cc46c81b54e7c7d1181f7b889f',1,'sparse_array']]]
+  ['zipper_0',['zipper',['../classzipper.html#a4e5c44163467e004c0a74338091bd36f',1,'zipper']]],
+  ['zipper_5fiterator_1',['zipper_iterator',['../classzipper__iterator.html#a30c5ed7c5ac4ca2939a27da46d5ac107',1,'zipper_iterator::zipper_iterator(const iterator_tuple &amp;it_tuple, size_t max, size_t idx)'],['../classzipper__iterator.html#a84b00d3f9fbd379844660abcdd4cf703',1,'zipper_iterator::zipper_iterator(const zipper_iterator &amp;z)']]]
 ];
