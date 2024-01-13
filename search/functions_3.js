b1fc323590e6297b0b9a069f85f3a778110d0159
ElioMinati <ElioMinati@users.noreply.github.com>
2024-01-13 18:07:36 +0000

    deploy: 628062351f76df4c4294166128ee3b155f03125f

--- search/functions_3.js
+++ search/functions_3.js
@@ -2,7 +2,12 @@ var searchData=
 [
   ['damage_0',['Damage',['../structcomponent_1_1Damage.html#aa6228f880ff1b2f13e857aac7d78379c',1,'component::Damage']]],
   ['deathevent_1',['DeathEvent',['../classDeathEvent.html#a189385a1aeb4aa4ada2dd3d9e9f2cdcf',1,'DeathEvent']]],
-  ['diff_2',['diff',['../classbasic__json.html#a699ef418df577e75f28dfce6b04d6c2f',1,'basic_json']]],
-  ['drawable_3',['Drawable',['../structcomponent_1_1Drawable.html#ae595642beaf9006a4544dd5e037ccbd3',1,'component::Drawable']]],
-  ['dump_4',['dump',['../classdetail_1_1serializer.html#ae73c1bac64bcc810923b9f1261af8b09',1,'detail::serializer::dump()'],['../classbasic__json.html#a85df48caed9e341bb14d98ab88891d1e',1,'basic_json::dump()']]]
+  ['decreaselives_2',['decreaseLives',['../classClient.html#ae31980599eef0c42f08d9755e08a4760',1,'Client']]],
+  ['diff_3',['diff',['../classbasic__json.html#a699ef418df577e75f28dfce6b04d6c2f',1,'basic_json']]],
+  ['displayfriends_4',['displayFriends',['../classServer.html#a1074d5db01c55f308431f1d5bdeaa005',1,'Server']]],
+  ['displayscoreboardmenu_5',['displayScoreBoardMenu',['../classClient.html#a1a37e6650eef7546f32d52b06776770c',1,'Client']]],
+  ['displaytexts_6',['displayTexts',['../classClient.html#a85e60f733164296d0c545597f0bd08be',1,'Client']]],
+  ['draw_7',['draw',['../classMenu.html#a1443dcfd1c1c9844c40658d752c1fbab',1,'Menu']]],
+  ['drawable_8',['Drawable',['../structcomponent_1_1Drawable.html#ae595642beaf9006a4544dd5e037ccbd3',1,'component::Drawable']]],
+  ['dump_9',['dump',['../classdetail_1_1serializer.html#ae73c1bac64bcc810923b9f1261af8b09',1,'detail::serializer::dump()'],['../classbasic__json.html#a85df48caed9e341bb14d98ab88891d1e',1,'basic_json::dump()']]]
 ];
