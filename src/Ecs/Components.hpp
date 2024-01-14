95ea1d4812bf1e18a224fd4da54b32adb621b3de
noah.tesson <noah.tesson@epitech.eu>
2024-01-14 18:23:31 +0100

    (ECS)[ADD] component gravity

--- src/Ecs/Components.hpp
+++ src/Ecs/Components.hpp
@@ -713,19 +713,12 @@ namespace component {
      * @brief Jump structure to to find out if an entity can be jump
      * 
     */
-    struct Jump {
-        /**
-         * @brief group id to refer to the correct parallax
-         * 
-         */
-        bool _jump;
+    struct Gravity {
         /**
          * @brief Construct a new Jump object
          * 
-         * @param group the group id of the parallax 
-         * @param index the z-index of the element for the parallax
          */
-        Jump(bool jump = true) : _jump(jump) {};
+        Gravity() {};
     };
 
 };
